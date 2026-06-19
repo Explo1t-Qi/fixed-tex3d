10f7245c4144a095c96d67ba724465416372067e
tex3d-attack <jj0004whitegmn@gmail.com>
2026-06-19 12:25:28 +0800

    Delete Docker_openvla directory

--- Docker_openvla/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Activate vla_render conda env and exec the command.
-# MUJOCO_GL / PYOPENGL_PLATFORM are already set via ENV in the Dockerfile.
-source /opt/conda/etc/profile.d/conda.sh
-conda activate vla_render
-exec "$@"
